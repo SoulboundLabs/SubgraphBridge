2af08c3ce6444afe45caf5eb98d1c6549903a8c2
mercuricchloride <mercuryprotocol@protonmail.com>
2022-11-16 13:49:57 -0700

    requestCIDs are being generated from real subgraph queries

--- test/QueryCreation.t.sol
+++ test/QueryCreation.t.sol
@@ -6,16 +6,73 @@ import "../src/SubgraphBridge.sol";
 
 contract QueryCreation is Test {
 
-    string public firstChunk = '{"query":"{\n  blocks(first: 10, block: {hash: \"';
-    bytes32 public firstChunkKeccak = 0xb687ba2c120a9296cefaf2881c9b7d896b9a826a08ee80633b3a1140b12a7ff8;
+    // for first query test
+    bytes32 public blockHash1 = 0xeee517bc8c2cdaf7b1a122161223fd9d29d25f9250b071c964d508c5a9cb0ee3;
+    bytes public onlineBlockHash1 = hex'307865656535313762633863326364616637623161313232313631323233666439643239643235663932353062303731633936346435303863356139636230656533';
+    bytes32 public requestCID1 = 0x200d785a4e650a6d55daec459392da7c1e22a3304710221a0b807e2260626aca;
 
-    string public lastChunk = '\"}) {\n    id\n    number\n  }\n}","variables":{}}';
-    bytes32 public lastChunkKeccak = 0xbbd94e461ba20804f06eea5ac0ec3d19c90fc7a7201f4977462a22bd9b6bcf96;
+    // for second query test
+    bytes32 public blockHash2 = 0x5b1dc013f92873fe9017f28612ef67a5b141e83f75f036d9cb7c8e1b8ef805be;
+    bytes public onlineBlockHash2 = hex'307835623164633031336639323837336665393031376632383631326566363761356231343165383366373566303336643963623763386531623865663830356265';
+    bytes32 public requestCID2 = 0x294d2e09b8935ecec4294e6e3cf8f0f047877f6d5ec6f7ccaf53ba48ce8e1f74;
 
-    bytes32 public blockHash = 0x39b93cc969f3ac620b896a128ee322dbba2e745fb2d6b8ba7218a275b9296648;
-    bytes32 public requestCID = 0x088e483c247ca4554b207bffe11fa57082e1d4ace993301ecfafe7b269114e28;
+    // default query first chunk hex
+    bytes public firstChunk = hex'7b227175657279223a227b5c6e2020626f6e6465724164646564732866697273743a2031302c20626c6f636b3a207b686173683a205c22';
+    // default query last chunk hex
+    bytes public lastChunk = hex'5c227d29207b5c6e2020202069645c6e20207d5c6e7d5c6e222c227661726961626c6573223a7b7d7d';
 
-    uint blockHashOffset =  strlen(firstChunk);
+    bytes public totalQueryHex1 = hex'7b227175657279223a227b5c6e2020626f6e6465724164646564732866697273743a2031302c20626c6f636b3a207b686173683a205c223078656565353137626338633263646166376231613132323136313232336664396432396432356639323530623037316339363464353038633561396362306565335c227d29207b5c6e2020202069645c6e20207d5c6e7d5c6e222c227661726961626c6573223a7b7d7d';
+
+    bytes public totalQueryHex2 = hex'7b227175657279223a227b5c6e2020626f6e6465724164646564732866697273743a2031302c20626c6f636b3a207b686173683a205c223078356231646330313366393238373366653930313766323836313265663637613562313431653833663735663033366439636237633865316238656638303562655c227d29207b5c6e2020202069645c6e20207d5c6e7d5c6e222c227661726961626c6573223a7b7d7d';
+
+    function testKeccakStrings() public {
+        string memory result = "request";
+
+        bytes32 hash = keccak256(abi.encodePacked(result));
+
+        assertEq(hash, 0x72859a6ae50aa97f593f23df1c78bb1fd78cfc493fcef64159d6486223196833);
+
+        string memory words = "SomeOtherQuiteLongStringLetsJustSeeHowThisWorks";
+
+        bytes32 wordHash = keccak256(abi.encodePacked(words));
+
+        assertEq(wordHash, 0x34bba2d182f6f70dcdc5d2962126622c60729eea6534226f87b35dd2bd3045d4);
+    }
+
+    // function testCreatingQuery() public {
+    //     bytes memory blockHashBytes = hex'3078333962393363633936396633616336323062383936613132386565333232646262613265373435666232643662386261373231386132373562393239363634386f';
+    //
+    //     bytes memory combined = bytes.concat(firstChunk,blockHashBytes,lastChunk);
+    //
+    //     bytes32 combinedHash = keccak256(combined);
+    //
+    //     bytes memory onlineCombined = hex'7b227175657279223a227b5c6e2020626c6f636b732866697273743a2031302c20626c6f636b3a207b686173683a205c223078333962393363633936396633616336323062383936613132386565333232646262613265373435666232643662386261373231386132373562393239363634386f5c227d29207b5c6e2020202069645c6e202020206e756d6265725c6e20207d5c6e7d5c6e222c227661726961626c6573223a7b7d7d';
+    //
+    //     assertEq(combined, onlineCombined);
+    //
+    //     assertEq(combinedHash, keccak256(onlineCombined));
+    // }
+
+    function testActualSubgraphQueries() public {
+      bytes memory bytesBlockHash1 = toHexBytes(blockHash1);
+      bytes memory generatedQuery1 = bytes.concat(firstChunk, bytesBlockHash1, lastChunk);
+      assertEq(generatedQuery1, totalQueryHex1);
+      assertEq(keccak256(generatedQuery1), requestCID1);
+
+      bytes memory bytesBlockHash2 = toHexBytes(blockHash2);
+      bytes memory generatedQuery2 = bytes.concat(firstChunk, bytesBlockHash2, lastChunk);
+      assertEq(generatedQuery2, totalQueryHex2);
+      assertEq(keccak256(generatedQuery2), requestCID2);
+    }
+
+    /*
+
+    BELOW THIS LINE IS JUST HELPER FUNCTIONS:
+
+    -----------------------------------------
+
+    */
+
 
     function strlen(string memory s) internal pure returns (uint256) {
         uint256 len;
@@ -53,17 +110,6 @@ contract QueryCreation is Test {
         res = string(result);
     }
 
-    function bytes32ToString(bytes32 _bytes32) public pure returns (string memory) {
-        uint8 i = 0;
-        while(i < 32 && _bytes32[i] != 0) {
-            i++;
-        }
-        bytes memory bytesArray = new bytes(i);
-        for (i = 0; i < 32 && _bytes32[i] != 0; i++) {
-            bytesArray[i] = _bytes32[i];
-        }
-        return string(bytesArray);
-    }
     function toHex16 (bytes16 data) internal pure returns (bytes32 result) {
     result = bytes32 (data) & 0xFFFFFFFFFFFFFFFF000000000000000000000000000000000000000000000000 |
           (bytes32 (data) & 0x0000000000000000FFFFFFFFFFFFFFFF00000000000000000000000000000000) >> 64;
@@ -78,65 +124,16 @@ contract QueryCreation is Test {
     result = bytes32 (0x3030303030303030303030303030303030303030303030303030303030303030 +
            uint256 (result) +
            (uint256 (result) + 0x0606060606060606060606060606060606060606060606060606060606060606 >> 4 &
-           0x0F0F0F0F0F0F0F0F0F0F0F0F0F0F0F0F0F0F0F0F0F0F0F0F0F0F0F0F0F0F0F0F) * 7);
+           // 0x0F0F0F0F0F0F0F0F0F0F0F0F0F0F0F0F0F0F0F0F0F0F0F0F0F0F0F0F0F0F0F0F) * 7);
+           0x0F0F0F0F0F0F0F0F0F0F0F0F0F0F0F0F0F0F0F0F0F0F0F0F0F0F0F0F0F0F0F0F) * 39);
     }
 
     function toHex (bytes32 data) public pure returns (string memory) {
         return string (abi.encodePacked ("0x", toHex16 (bytes16 (data)), toHex16 (bytes16 (data << 128))));
     }
 
-    function testBlockHashToString() public {
-     string memory blockHashAsString = "0x39b93cc969f3ac620b896a128ee322dbba2e745fb2d6b8ba7218a275b9296648";
-     emit log_string(blockHashAsString);
-    }
-
-    function testKeccakStrings() public {
-        string memory result = "request";
-
-        bytes32 hash = keccak256(abi.encodePacked(result));
-
-        assertEq(hash, 0x72859a6ae50aa97f593f23df1c78bb1fd78cfc493fcef64159d6486223196833);
-
-        string memory words = "SomeOtherQuiteLongStringLetsJustSeeHowThisWorks";
-
-        bytes32 wordHash = keccak256(abi.encodePacked(words));
-
-        assertEq(wordHash, 0x34bba2d182f6f70dcdc5d2962126622c60729eea6534226f87b35dd2bd3045d4);
-
-        bytes memory first = hex'7b227175657279223a227b5c6e2020626c6f636b732866697273743a2031302c20626c6f636b3a207b686173683a205c22';
-
-        bytes32 firstChunkHash = keccak256(first);
-
-        assertEq(firstChunkHash, firstChunkKeccak);
-
-        bytes memory last = hex'5c227d29207b5c6e2020202069645c6e202020206e756d6265725c6e20207d5c6e7d222c227661726961626c6573223a7b7d7d';
-
-        bytes32 lastChunkHash = keccak256(last);
-
-        assertEq(lastChunkHash, lastChunkKeccak);
-    }
-
-    function testCreatingQuery() public {
-        bytes memory first = hex'7b227175657279223a227b5c6e2020626c6f636b732866697273743a2031302c20626c6f636b3a207b686173683a205c22';
-
-        bytes memory last = hex'5c227d29207b5c6e2020202069645c6e202020206e756d6265725c6e20207d5c6e7d5c6e222c227661726961626c6573223a7b7d7d';
-
-        // TODO: WE NEED A FUNCTION THAT CAN TURN A BLOCK HASH INTO IT'S TEXT BYTES ENCODING
-        bytes memory blockHashBytes = hex'3078333962393363633936396633616336323062383936613132386565333232646262613265373435666232643662386261373231386132373562393239363634386f';
-
-        bytes memory combined = bytes.concat(first,blockHashBytes,last);
-
-        bytes32 combinedHash = keccak256(combined);
-
-        bytes memory onlineCombined = hex'7b227175657279223a227b5c6e2020626c6f636b732866697273743a2031302c20626c6f636b3a207b686173683a205c223078333962393363633936396633616336323062383936613132386565333232646262613265373435666232643662386261373231386132373562393239363634386f5c227d29207b5c6e2020202069645c6e202020206e756d6265725c6e20207d5c6e7d5c6e222c227661726961626c6573223a7b7d7d';
-
-        assertEq(combined, onlineCombined);
-
-        assertEq(combinedHash, keccak256(onlineCombined));
-
-        // assertEq(combinedHash, 0x7c7661153902e08e05f1830562654324a58541af2e63c0c0b64d4120e4dece15);
-
-        // assertEq(keccak256(onlineCombined), hex'7c7661153902e08e05f1830562654324a58541af2e63c0c0b64d4120e4dece15');
+    function toHexBytes (bytes32 data) public pure returns (bytes memory) {
+        return abi.encodePacked ("0x", toHex16 (bytes16 (data)), toHex16 (bytes16 (data << 128)));
     }
 
 }
